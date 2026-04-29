24023947c7278ee45949959865a858ba480239d3
ahed666 <ahed0naser0eddin@gmail.com>
2026-04-29 11:45:31 +0400

    add vercel analytics

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import Header from './components/Header.vue';
 import Footer from './components/Footer.vue';
+import { Analytics } from "@vercel/analytics/vue";
 </script>
 
 <template>
